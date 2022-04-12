3aa61c510e2c2d6fc01097010a77a84cd479e5d4
Anjunheon <jun02072001@gmail.com>
2022-04-12 21:03:03 +0900

    20220412 update

--- BOJ/14888.py
+++ BOJ/14888.py
@@ -6,11 +6,9 @@ print = stdout.write
 minval = 1000000000
 maxval = -1000000000
 
-cnt = 0
 
-
-def dfs(Op, res):
-    global minval, maxval, cnt
+def dfs(Op, res, cnt):
+    global A, minval, maxval
     cnt += 1
 
     if sum(Op) == 0:
@@ -18,27 +16,31 @@ def dfs(Op, res):
         maxval = max(maxval, res)
 
     if Op[0] > 0:
-        Op[0] -= 1
-        dfs(Op, res + A[cnt])
+        Op1 = Op.copy()
+        Op1[0] -= 1
+        dfs(Op1, res + A[cnt], cnt)
     if Op[1] > 0:
-        Op[1] -= 1
-        dfs(Op, res - A[cnt])
+        Op2 = Op.copy()
+        Op2[1] -= 1
+        dfs(Op2, res - A[cnt], cnt)
     if Op[2] > 0:
-        Op[2] -= 1
-        dfs(Op, res * A[cnt])
+        Op3 = Op.copy()
+        Op3[2] -= 1
+        dfs(Op3, res * A[cnt], cnt)
     if Op[3] > 0:
-        Op[3] -= 1
-        if res < 0:
-            dfs(Op, -(abs(res) // A[cnt]))
-        if res > 0:
-            dfs(Op, abs(res) / A[cnt])
+        Op4 = Op.copy()
+        Op4[3] -= 1
+        if res < 0 and A[cnt] > 0:
+            dfs(Op4, -int(abs(res) / A[cnt]), cnt)
+        else:
+            dfs(Op4, res // A[cnt], cnt)
 
 
 N = input()
 A = list(map(int, input().split()))
 Op = list(map(int, input().split()))
 
-dfs(Op, A[0])
+dfs(Op, A[0], 0)
 
 print(str(maxval) + '\n')
 print(str(minval))
