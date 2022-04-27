bbd2cdefb6f4bbae9b81999a736ee9ac991f66a0
Anjunheon <jun02072001@gmail.com>
2022-04-27 19:40:36 +0900

    20220427 update *

--- BOJ/1806.py
+++ BOJ/1806.py
@@ -4,16 +4,24 @@ input = stdin.readline
 print = stdout.write
 
 N, S = map(int, input().split())
-
 num = list(map(int, input().split()))
 
-if sum(num) < S:
+answer = 100001
+
+start = end = total = 0
+
+while True:
+    if total >= S:
+        answer = min(answer, end - start)
+        total -= num[start]
+        start += 1
+    elif end == N:
+        break
+    else:
+        total += num[end]
+        end += 1
+
+if answer > N:
     print(str(0))
-    exit()
-
-for i in range(N):
-    for j in range(N-i):
-        if sum(num[j:j+i+1]) >= S:
-            print(str(i+1))
-            exit()
-
+else:
+    print(str(answer))
